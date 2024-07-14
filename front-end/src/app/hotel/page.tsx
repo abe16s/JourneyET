a9534ded5bc84bd5747c36786324e691081aa1bc
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-07-14 21:05:19 +0300

    Hotels page

---
+++ front-end/src/app/hotel/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Hotel from "../../components/Hotel/Hotel"
+
+export default function page() {
+    return (
+        <Hotel/>
+    )
+}
--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"></link>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
