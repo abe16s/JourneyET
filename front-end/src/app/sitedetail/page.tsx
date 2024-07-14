0ce6912e849498cf7e54f40fdeae8a871d8f1032
Leykun Birhanu <leykunbi12@gmail.com>
2024-07-15 00:54:33 +0300

    added the experience section in details page

--- front-end/src/app/sitedetail/page.tsx
+++ front-end/src/app/sitedetail/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import SiteDetail from "../../components/SiteDetail/SiteDetail"
-
+import Experience from "../../components/SiteDetail/Experience"
 export default function page() {
     return (
-        <SiteDetail/>
+        <>
+            <SiteDetail/>
+            <Experience/>
+        </>
+        
     )
 }
---
+++ front-end/src/components/SiteDetail/EachExperience.js
@@ -0,0 +1,13 @@
+export default function Testimonials(){
+  return(
+    <>
+        <div className="flex flex-col items-center justify-center w-[412px] gap-y-4 border-2 rounded-2xl ml-10 mb-7">
+          <div className="w-24 h-24 mt-6">
+            <img src="Haile-1.jpg" className="w-full h-full rounded-full object-cover"></img>
+          </div>
+          <div className="text-left font-semibold">Leykun B.</div>
+          <div className="ml-6 mr-4 mb-6">Lorem ipsum dolor sit amet, consect adipiscing elit. Nunc vel velit a est placerat dictum et et libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices </div>
+        </div>   
+    </>
+  );
+};
