b44322b84dfe90b25a4cedee3bc58bb72c24167d
Leykun Birhanu <leykunbi12@gmail.com>
2024-07-17 09:33:30 +0300

    responsive made for the hotel,experience,tourguide cards except local culture card

--- front-end/src/components/SiteDetail/Comment.js
+++ front-end/src/components/SiteDetail/Comment.js
@@ -1,19 +1,8 @@
-"use client"
-
-import { useState } from 'react';
 export default function Review(){
-  const [reviewStar, setReviewStar] = useState(0);
   return(
   <div className='ml-10'>
-    <h4 className="font-bold text-2xl">Leave a review</h4>
-    <div className="flex gap-2 my-5">
-        <i className={`fa-star ${reviewStar >= 1 ? 'fa-solid': "fa-regular"}`} onClick={() => {setReviewStar(1)}}></i>
-        <i className={`fa-star ${reviewStar >= 2 ? 'fa-solid': "fa-regular"}`} onClick={() => {setReviewStar(2)}}></i>
-        <i className={`fa-star ${reviewStar >= 3 ? 'fa-solid': "fa-regular"}`} onClick={() => {setReviewStar(3)}}></i>
-        <i className={`fa-star ${reviewStar >= 4 ? 'fa-solid': "fa-regular"}`} onClick={() => {setReviewStar(4)}}></i>
-        <i className={`fa-star ${reviewStar >= 5 ? 'fa-solid': "fa-regular"}`} onClick={() => {setReviewStar(5)}}></i>
-    </div>
-    <textarea rows={5} cols={60} placeholder="Enter your comments here..." className="border-2 border-black rounded-md p-2 block"/>
+    <h4 className="font-bold text-2xl">Share your experience</h4>
+    <textarea rows={5} cols={60} placeholder="Enter your experience here..." className="border-2 border-black rounded-md p-2 block mt-4 max-w-full"/>
     <button className="bg-yellow-200 font-bold py-1 px-5 mt-3 rounded-xl w-max">Post</button>
   </div>
   );
